bcc20fd6671332e76b58a22c59f1363dd270b5b9
mp <mariusz.piatek92@gmail.com>
2017-06-12 22:45:15 +0200

    Added github bridge mock

--- spec/mrpiatek/RepoLookup/RepositoryLookup/DataFetchers/GitHubDataFetcherSpec.php
+++ spec/mrpiatek/RepoLookup/RepositoryLookup/DataFetchers/GitHubDataFetcherSpec.php
@@ -2,6 +2,8 @@
 
 namespace spec\mrpiatek\RepoLookup\RepositoryLookup\DataFetchers;
 
+use Github\Exception\RuntimeException;
+use GrahamCampbell\GitHub\GitHubManager;
 use mrpiatek\RepoLookup\RepositoryLookup\DataFetchers\GitHubDataFetcher;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -9,6 +11,34 @@ use mrpiatek\RepoLookup\RepositoryLookup\Exceptions\RepositoryNotFoundException;
 
 class GitHubDataFetcherSpec extends ObjectBehavior
 {
+    function let(GitHubManager $gitHubManager)
+    {
+        $gitHubManager->api("repo")->willReturn(new class
+        {
+            public function contributors($vendor, $package)
+            {
+                if ($vendor == 'laravel' && $package == 'laravel') {
+                    return [
+                        [
+                            'login' => 'taylorotwell',
+                            'avatar_url' => 'https://avatars3.githubusercontent.com/u/463230',
+                            'contributions' => 2953
+                        ],
+                        [
+                            'login' => 'GrahamCampbell',
+                            'avatar_url' => 'https://avatars0.githubusercontent.com/u/2829600',
+                            'contributions' => 40
+                        ]
+                    ];
+                } else {
+                    throw new RuntimeException();
+                }
+            }
+        });
+
+        $this->beConstructedWith($gitHubManager);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType(GitHubDataFetcher::class);
@@ -16,7 +46,7 @@ class GitHubDataFetcherSpec extends ObjectBehavior
 
     function it_looks_up_repository_contributors()
     {
-        $this->fetchRepositoryData('laravel/laravel')->shouldContain(
+        $this->fetchRepositoryData('laravel', 'laravel')->shouldContain(
             [
                 'name' => 'taylorotwell',
                 'avatar_url' => 'https://avatars3.githubusercontent.com/u/463230',
@@ -24,7 +54,7 @@ class GitHubDataFetcherSpec extends ObjectBehavior
             ]
         );
 
-        $this->fetchRepositoryData('laravel/laravel')->shouldContain(
+        $this->fetchRepositoryData('laravel', 'laravel')->shouldContain(
             [
                 'name' => 'GrahamCampbell',
                 'avatar_url' => 'https://avatars0.githubusercontent.com/u/2829600',
@@ -36,6 +66,6 @@ class GitHubDataFetcherSpec extends ObjectBehavior
     function it_fails_with_not_existing_repository()
     {
         $this->shouldThrow(RepositoryNotFoundException::class)
-            ->duringFetchRepositoryData('mrpiatek/null');
+            ->duringFetchRepositoryData('mrpiatek', 'null');
     }
 }
