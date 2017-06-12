42f698d5d3ac5c9f224dcf97e149352d1647b3a1
mp <mariusz.piatek92@gmail.com>
2017-06-12 20:13:23 +0200

    Added basic unit test for repository lookup class and generated its stub

---
+++ spec/mrpiatek/RepoLookup/RepositoryLookup/RepositoryLookupSpec.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace spec\mrpiatek\RepoLookup\RepositoryLookup;
+
+use mrpiatek\RepoLookup\RepositoryLookup\RepositoryLookup;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class RepositoryLookupSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(RepositoryLookup::class);
+    }
+
+    function it_looks_up_repository_contributors()
+    {
+        $this->lookupRepository('laravel/laravel')->shouldContain(
+            [
+                'name' => 'taylorotwell',
+                'avatar_url' => 'https://avatars3.githubusercontent.com/u/463230',
+                'contributions' => 2953
+            ]
+        );
+        $this->lookupRepository('laravel/laravel')->shouldContain(
+            [
+                'name' => 'GrahamCampbell',
+                'avatar_url' => 'https://avatars0.githubusercontent.com/u/2829600',
+                'contributions' => 40
+            ]
+        );
+    }
+}
---
+++ src/mrpiatek/RepoLookup/RepositoryLookup/RepositoryLookup.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * PHP version 7
+ *
+ * Repository lookup class
+ *
+ * Class used to lookup repository contributors and present the list with avatars
+ * and number of contributions.
+ *
+ * @category Core
+ * @package  MrpiatekRepositoryLookup
+ * @author   Mariusz Piątek <mariusz.piatek92@gmail.com>
+ * @license  MIT <https://opensource.org/licenses/MIT>
+ * @link     https://github.com/mrpiatek/repo-lookup/src/mrpiatek/RepoLookup/RepositoryLookup/RepositoryLookup.php
+ */
+
+namespace mrpiatek\RepoLookup\RepositoryLookup;
+
+/**
+ * Class RepositoryLookup
+ *
+ * @category Core
+ * @package  MrpiatekRepositoryLookup
+ * @author   Mariusz Piątek <mariusz.piatek92@gmail.com>
+ * @license  MIT <https://opensource.org/licenses/MIT>
+ * @link     https://github.com/mrpiatek/repo-lookup/src/mrpiatek/RepoLookup/RepositoryLookup/RepositoryLookup.php
+ */
+class RepositoryLookup
+{
+    /**
+     * Fetches information about repository contributors and returns it as an array
+     *
+     * @param string $repositoryName Full name of the repository
+     *
+     * @return array
+     */
+    public function lookupRepository(string $repositoryName): array
+    {
+        //TODO: implement
+    }
+}
